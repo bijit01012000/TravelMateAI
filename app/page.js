40df687055f7c1b2425cc1eab66a4ddb83dbfb7e
bijit sahu <bijitsahu225@gmail.com>
2025-05-03 18:07:30 +0530

    final touch

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-6xl font-bold text-primary">GPTGenius </h1>
+          <h1 className="text-6xl font-bold text-primary">Travel Mate </h1>
           <p className="py-6 text-lg leading-loose">
             TravelMateAI: Your AI language companion. Powered by OpenAI, it A
             smart travel companion designed to guide you through any city,
